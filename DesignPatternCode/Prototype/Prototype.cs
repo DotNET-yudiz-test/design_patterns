eff172cec9a62aad8558072cf22263a864dbdf7f
kunal vachhani <kunal.v@yudiz.in>
2020-12-10 11:20:40 +0000

    Update Prototype.cs

--- DesignPatternCode/Prototype/Prototype.cs
+++ DesignPatternCode/Prototype/Prototype.cs
@@ -7,7 +7,7 @@ namespace DoFactory.GangOfFour.Prototype.RealWorld
     //in other words it allows us to create a new instance by copying existing instances.
     public class Developer : ICloneable
     {
-        public string First Name { get; set; }
+        public string FirstName { get; set; }
         public string Lastname { get; set; }
         public List<string> Skills { get; set; }
 
